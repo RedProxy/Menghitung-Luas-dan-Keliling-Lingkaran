1cef55319bb664bf9d02d39336077796ee33c563
RedProxy <alwinrfthh@gmail.com>
2021-10-29 05:27:55 +0700

    menambahkan file

--- menghitung_luas_dan_keliling_lingkaran.py
+++ menghitung_luas_dan_keliling_lingkaran.py
@@ -1,13 +1,14 @@
+print("Program Menghitung Luas dan Keliling Lingkaran")
+print("==============================================")
+r = float(input("Masukan Jari-Jari Lingkaran  : "))
+
 phi = 3.14
+luas = phi*r*r
+kel = 2*phi*r
 
-
-r = float(input("Masukan jari-jari lingkaran : "))
-
-luas = phi*(r*r)
-keliling = 2*phi*r
 print ()
 print ("Rumus Luas Lingkarannya \t=",phi,"x",r,"x",r,)
-print ("Rumus KelilingbLingkaran \t=",2,"x",phi,"x",r,)
+print ("Rumus Keliling Lingkaran \t=",2,"x",phi,"x",r,)
 print ()
-print ("Hasil Luas Lingkaran \t\t= ",format(luas,'.2f'))
-print ("Hasil Keliling Lingkaran\t= ",format(keliling,'.2f'))
+print ("Hasil Luas Lingkaran :",format(luas,'.2f'))
+print ("Hasil Keliling Lingkaran :",format(kel,'.2f'))
